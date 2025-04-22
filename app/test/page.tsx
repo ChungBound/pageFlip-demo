6ad4f4fc8937c5530fa8dab256a48b3cdf439562
BunnyZhang <a1904674@adelaide.edu.au>
2025-04-22 22:04:16 +0930

    Add comment for 'use client' delete useless components Add test page

---
+++ app/test/page.tsx
@@ -0,0 +1,9 @@
+"use client"
+import { FinalBook } from "@/components/final-book"
+export default function TestPage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <FinalBook />
+    </div>
+  )
+} 
--- components/auto-left-turn-book.tsx
+++ components/auto-left-turn-book.tsx
@@ -1,5 +1,7 @@
 "use client"
-
+// 是的,加上"use client"就是CSR(客户端渲染)模式,组件在浏览器端执行,可以使用浏览器API和React hooks
+// 不加的话默认是SSR(服务器端渲染)模式,组件在Node.js服务器上预渲染,不能使用客户端特性
+// 这是Next.js 13+ App Router的新特性,用于优化性能和SEO
 import { BookComponent } from "./book-component"
 
 export function AutoLeftTurnBook() {
